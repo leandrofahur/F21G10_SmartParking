8ba36d6ae15ad461bc77a5fb1fb333f718d0c532
Mariana Magalhães <maari.magalhaes@hotmail.com>
2021-11-25 23:25:53 -0800

    user view ids and strings

--- SmartParking/app/src/main/java/com/example/smartparking/activities/UserActivity.java
+++ SmartParking/app/src/main/java/com/example/smartparking/activities/UserActivity.java
@@ -4,20 +4,39 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.GridView;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.example.smartparking.R;
 
 public class UserActivity extends AppCompatActivity {
 
-    //private Button userBtn;
+    private TextView welcomeTxtView;
+    private TextView userNameTextView;
+    private GridView parkingLotGridView;
+    private TextView selectCategoryTxtView;
+    private ImageButton carsImgBtn;
+    private ImageButton bikeImgBtn;
+    private ImageButton vanImgBtn;
+    private ImageButton disabledImgBtn;
+    private Button submitBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
 
-        //userBtn = findViewById(R.id.btnTest);
+        welcomeTxtView = findViewById(R.id.txtViewWelcome);
+        userNameTextView = findViewById(R.id.txtViewNameUser); //get username from input on login
+
+        parkingLotGridView = findViewById(R.id.gridViewParkingLot);
+        selectCategoryTxtView = findViewById(R.id.txtViewSelectCategory);
+        carsImgBtn = findViewById(R.id.imgBtnCars);
+        bikeImgBtn = findViewById(R.id.imgBtnBike);
+        vanImgBtn = findViewById(R.id.imgBtnVan);
+        disabledImgBtn = findViewById(R.id.imgBtnDisabled);
+        submitBtn = findViewById(R.id.btnSubmit);
     }
 
 }
