3d8fa01e92dce347877002fd47bbe7e61ef07854
Mariana Magalhães <maari.magalhaes@hotmail.com>
2021-11-26 00:09:19 -0800

    user updates activity and xml

--- SmartParking/app/src/main/java/com/example/smartparking/activities/UserActivity.java
+++ SmartParking/app/src/main/java/com/example/smartparking/activities/UserActivity.java
@@ -4,16 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.GridView;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
 import com.example.smartparking.R;
+import com.google.android.material.textfield.TextInputLayout;
 
 public class UserActivity extends AppCompatActivity {
 
     private TextView welcomeTxtView;
     private TextView userNameTextView;
+    //private TextView inputText;
     private GridView parkingLotGridView;
     private TextView selectCategoryTxtView;
     private ImageButton carsImgBtn;
@@ -22,13 +25,17 @@ public class UserActivity extends AppCompatActivity {
     private ImageButton disabledImgBtn;
     private Button submitBtn;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
 
         welcomeTxtView = findViewById(R.id.txtViewWelcome);
-        userNameTextView = findViewById(R.id.txtViewNameUser); //get username from input on login
+        userNameTextView = findViewById(R.id.txtViewNameUser); //get username from input on login to set text in user name textview
+
+        //inputText = findViewById(R.id.textViewLayoutUsername);
+        //userNameTextView.setText(inputText.getText());
 
         parkingLotGridView = findViewById(R.id.gridViewParkingLot);
         selectCategoryTxtView = findViewById(R.id.txtViewSelectCategory);
