00a1087c10b330a8057fc7613b8316eece765c7f
Henrique Barros <rodriguesdebarrosh@student.douglascollege.ca>
2021-11-29 21:03:57 -0800

    Created Invoice class, DAO, ViewModel, Repository and made changes to the DB

--- SmartParking/app/src/main/java/com/example/smartparking/database/SmartParkingRoomDB.java
+++ SmartParking/app/src/main/java/com/example/smartparking/database/SmartParkingRoomDB.java
@@ -8,18 +8,19 @@ import androidx.room.Room;
 import androidx.room.RoomDatabase;
 import androidx.sqlite.db.SupportSQLiteDatabase;
 
+import com.example.smartparking.dao.InvoiceDAO;
 import com.example.smartparking.dao.UserDAO;
+import com.example.smartparking.model.Invoice;
 import com.example.smartparking.model.User;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-@Database(entities = {User.class}, version = 1, exportSchema = false)
+@Database(entities = {User.class, Invoice.class}, version = 1, exportSchema = false)
 public abstract class SmartParkingRoomDB extends RoomDatabase {
     // insert the dao's:
     public abstract UserDAO userDAO();
-
-
+    public abstract InvoiceDAO invoiceDAO();
 
     /*
      * Singleton Pattern:
@@ -56,9 +57,16 @@ public abstract class SmartParkingRoomDB extends RoomDatabase {
                 UserDAO userDAO = INSTANCE.userDAO();
                 userDAO.deleteAllUsers();
 
+                InvoiceDAO invoiceDAO = INSTANCE.invoiceDAO();
+                invoiceDAO.deleteAllInvoices();
+
                 // insert a default user:
                 User user = new User("douglas@college.ca","123");
                 userDAO.insertUser(user);
+
+                // insert dummy invoice:
+                Invoice invoice = new Invoice("Test invoice", 1, "Mon Nov 29 08:52:30 2021", "Mon Nov 29 09:52:30 2021");
+                invoiceDAO.insertInvoice(invoice);
             });
         }
     };
